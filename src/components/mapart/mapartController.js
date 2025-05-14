c1e0bb1097d435db524a79cfdf9d42da50445457
Gray <octo3535@aol.com>
2025-05-14 01:59:03 -0700

    Update mapartController.js

--- src/components/mapart/mapartController.js
+++ src/components/mapart/mapartController.js
@@ -571,6 +571,9 @@ class MapartController extends Component {
         document.body.style.backgroundSize="100%";
         fetch("https://derpibooru.org/api/v1/json/search/images?q=scenery,score.gte:1000,safe&sf=random&per_page=1").then(req=>req.json()).then(derp=>document.body.style.backgroundImage=`url(${derp.images[0].representations.full})`);
         return null;
+      default:
+    console.error('Unexpected option: ', someVariable);  // Handle the default case
+}
     }
     if (!/^[0-9a-zQ-ZA-P]*$/g.test(encodedPreset)) {
       onCorruptedPreset();
